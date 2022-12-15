f47e62fe15c98a2dd38ebc8e7c47f42967334b96
Krishan-Kumar-Prajapati <krishan.kumar@alumni.iitgn.ac.in>
2022-12-15 11:53:06 +0530

    Day-2

---
+++ script.js
@@ -0,0 +1,16 @@
+let slideshow=document.getElementById("slideshow");
+
+
+let arrslideshow=["https://lmsin.net/cdn-cgi/image/w=1232,q=70,fit=cover/https://70415bb9924dca896de0-34a37044c62e41b40b39fcedad8af927.lmsin.net/LS-Fest/LS-new/LS-Uber-HP-Desktop-HeroBanner6-14Dec2022.gif","https://lmsin.net/cdn-cgi/image/w=1232,q=70,fit=cover/https://70415bb9924dca896de0-34a37044c62e41b40b39fcedad8af927.lmsin.net/LS-Fest/LS-new/LS-Banner3-desktop-ShoeandBags-14Dec2022.jpg","https://lmsin.net/cdn-cgi/image/w=1232,q=70,fit=cover/https://70415bb9924dca896de0-34a37044c62e41b40b39fcedad8af927.lmsin.net/LS-Fest/LS-new/LS-Banner2-desktop-ShoeandBags-14Dec2022.jpg","https://lmsin.net/cdn-cgi/image/w=1232,q=70,fit=cover/https://70415bb9924dca896de0-34a37044c62e41b40b39fcedad8af927.lmsin.net/LS-Fest/LS-new/HP_LS_Common_Desktop-1-14Dec2022.gif","https://lmsin.net/cdn-cgi/image/w=1232,q=70,fit=cover/https://70415bb9924dca896de0-34a37044c62e41b40b39fcedad8af927.lmsin.net/LS-Fest/LS-new/LS-Banner5-desktop-ShoeandBags-14Dec2022.jpg","https://lmsin.net/cdn-cgi/image/w=1232,q=70,fit=cover/https://70415bb9924dca896de0-34a37044c62e41b40b39fcedad8af927.lmsin.net/LS-Fest/LS-new/LS-Banner4-desktop-ShoeandBags-14Dec2022.jpg","https://lmsin.net/cdn-cgi/image/w=1232,q=70,fit=cover/https://70415bb9924dca896de0-34a37044c62e41b40b39fcedad8af927.lmsin.net/LS-Fest/LS-new/LS-Uber-HP-Desktop-HeroBanner5-14Dec2022.jpg"];
+let i=0;
+setInterval(function(){
+    slideshow.innerHTML=null;
+    i++;
+    if(i==arrslideshow.length){
+        i=0;
+    }
+let myimg=document.createElement("img");
+myimg.setAttribute("src",arrslideshow[i]);
+slideshow.append(myimg);
+
+},5000);
